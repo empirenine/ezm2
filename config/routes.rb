3be8816c0e50019138eacf91cc1c76e264626ead
Mike MacDonald <empirenine@gmail.com>
2012-10-25 15:14:55 -0700

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Easymse2::Application.routes.draw do
   resources :entries
 
-  resources :clients # do
-    # resources :entries, :except => [:show, :edit, :update] do
-    # end
-  # end
-  # get 'clients/:client_id/entries/:id' => 'entries#show', :as => 'client_entry'
-  # get 'clients/:client_id/entries/:id/edit' => 'entries#edit', :as => 'edit_client_entry'
-  # put 'clients/:client_id/entries/:id' => 'entries#update', :as => 'client_entry'
-  # delete 'clients/:client_id/entries/:id' => 'entries#destroy'
-  
+  resources :clients
+
   authenticated :user do
     root :to => 'home#index'
   end
