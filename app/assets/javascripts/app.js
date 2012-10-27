9277540b8d032029fc1fe2d26bd033c5b0189569
Mike MacDonald <empirenine@gmail.com>
2012-10-27 13:04:14 -0700

    Fixed private data hiding function

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   
   // Allows provider to toggle the visibility of sensitive information on their screen
   
-  $(".show-private-data").click(function() {
-    $("#body").toggleClass("shown");
+  $("#show-private-data").click(function() {
+    $('#body').toggleClass("shown");
     var text = $(this).text();
     $(this).text(text == "Hide sensitive data" ? "Show sensitive data" : "Hide sensitive data");
   });
