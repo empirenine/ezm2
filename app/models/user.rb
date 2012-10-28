1a0112ee5b72e6a2f84d40552410071664e50b3e
Mike MacDonald <empirenine@gmail.com>
2012-10-27 17:59:20 -0700

    Added has many to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ActiveRecord::Base
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
-  # has_many :clients
+  has_many :clients
+  
   before_validation :whitelisted
 
   def whitelisted
