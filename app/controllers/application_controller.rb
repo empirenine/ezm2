0a36069d65c06d12c92c088eaeb7c684fd578f9a
Mike MacDonald <empirenine@gmail.com>
2012-10-28 08:38:23 -0700

    Removed login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,5 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
-  
-  before_filter :require_login
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_user_session_url # halts request cycle
-    end
-  end
-
-  def logged_in?
-    !!current_user
-  end
 
 end
